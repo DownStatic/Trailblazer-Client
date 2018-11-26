35dfab84697c99b20c3b699233219cf613ecdf46
Chris Stull <chris.stull@gmail.com>
2018-11-26 17:42:29 -0500

    User form submission working.

--- src/containers/Signup.jsx
+++ src/containers/Signup.jsx
@@ -1,13 +1,15 @@
 import React, { PureComponent } from 'react'
+import '../assets/scss/Signup.scss'
 
 export default class Signup extends PureComponent {
 
   state = {
-    username: "",
+    name: "",
     password: "",
     age: 0,
     proficiency: "beginner",
-    address: {}
+    address: {},
+    avatar: null
   }
 
   handleChange = (event) => {
@@ -27,6 +29,20 @@ export default class Signup extends PureComponent {
     }})
   }
 
+  handleSubmit = () => {
+    let avatar = document.getElementById("avatar_file").files[0]
+    let form_upload = new FormData()
+    form_upload.append("avatar", avatar)
+    let not_avatar = {...this.state}
+    delete not_avatar["avatar"]
+    form_upload.append("user", JSON.stringify(not_avatar))
+    console.log(not_avatar);
+    fetch('http://localhost:3000/api/v1/users', {
+      method: "POST",
+      body: form_upload
+    }).then(res => res.json()).then(json => console.log(json))
+  }
+
   proficiencySelect = () => {
     return(
       <select onChange={this.handleChange} name="proficiency">
@@ -37,26 +53,37 @@ export default class Signup extends PureComponent {
     )
   }
 
+  avatarUpload = (event) => {
+    this.setState({avatar: URL.createObjectURL(event.target.files[0])})
+  }
+
   render(){
     return(
-      <div>
-        <label>Username</label><br></br>
-        <input onChange={this.handleChange} name="username" type="text" placeholder="your username"></input><br></br>
-        <label>Password</label><br></br>
-        <input onChange={this.handleChange} name="password" type="text" placeholder="your password"></input><br></br>
-        <label>Age</label><br></br>
-        <input onChange={this.handleChange} name="age" type="number"></input><br></br>
-        <label>Select Your Proficiency</label><br></br>
-        {this.proficiencySelect()}<br></br>
-        <label>Street Address</label><br></br>
-        <input onChange={this.handleAddress} name="street_address" type="text"></input><br></br>
-        <label>City</label><br></br>
-        <input onChange={this.handleAddress} name="city" type="text"></input><br></br>
-        <label>State</label><br></br>
-        <input onChange={this.handleAddress} name="state" type="text"></input><br></br>
-        <label>Zip Code</label><br></br>
-        <input onChange={this.handleAddress} name="zip_code" type="number"></input><br></br>
-        <button onClick={this.createUser}>Submit</button>
+      <div className="user_creation">
+        <div className="avatar_portion">
+          <label>Avatar</label>
+          <input onChange={this.avatarUpload} name="avatar" id="avatar_file" type="file" accept="image/*"></input><br></br>
+          <img src={this.state.avatar ? this.state.avatar : "https://is3-ssl.mzstatic.com/image/thumb/Purple91/v4/f6/9f/62/f69f62cc-ccf2-cc72-b7f2-5ba8233d9ed9/source/256x256bb.jpg"} alt="questionable" />
+        </div>
+        <div className="user_details">
+          <label>Username</label><br></br>
+          <input onChange={this.handleChange} name="name" type="text" placeholder="your username"></input><br></br>
+          <label>Password</label><br></br>
+          <input onChange={this.handleChange} name="password" type="text" placeholder="your password"></input><br></br>
+          <label>Age</label><br></br>
+          <input onChange={this.handleChange} name="age" type="number"></input><br></br>
+          <label>Select Your Proficiency</label><br></br>
+          {this.proficiencySelect()}<br></br>
+          <label>Street Address</label><br></br>
+          <input onChange={this.handleAddress} name="street_address" type="text"></input><br></br>
+          <label>City</label><br></br>
+          <input onChange={this.handleAddress} name="city" type="text"></input><br></br>
+          <label>State</label><br></br>
+          <input onChange={this.handleAddress} name="state" type="text"></input><br></br>
+          <label>Zip Code</label><br></br>
+          <input onChange={this.handleAddress} name="zip_code" type="number"></input><br></br>
+          <button onClick={this.handleSubmit}>Submit</button>
+        </div>
       </div>
     )
   }
